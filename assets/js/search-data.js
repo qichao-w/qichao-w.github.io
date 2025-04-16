f93fa71adfedf6534914df431692cd9cfeb2e379
qichao-w <qichaoswang@gmail.com>
2025-04-16 14:11:24 +0000

    Deploying to gh-pages from @ qichao-w/qichao-w.github.io@ddcfffde01a1308cd001786b5936ab679d86b7d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,6 +59,17 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
+      },{id: "post-a-post-with-formatting-and-links",
+        
+          title: "a post with formatting and links",
+        
+        description: "march &amp; april, looking forward to summer",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/formatting-and-links/";
+          
+        },
       },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
